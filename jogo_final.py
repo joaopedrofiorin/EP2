af6104e0832380bc52456fccefff8ccacc92b152
Joao Pedro <joaoprf4@al.insper.edu.br>
2021-05-07 11:08:01 -0300

    jogo em processo

--- jogo_final.py
+++ jogo_final.py
@@ -22,3 +22,39 @@ print(' ')
 print(input('Aperte ENTER para começar o jogo: '))
 print(' ')
 print('O estado do baralho atual é: ')
+
+baralho = cria_baralho()
+i = 1
+for carta in baralho:
+    print('{}. {}'.format(i,carta))
+    i += 1
+
+verifica = possui_movimentos_restantes(baralho)
+
+while verifica:
+    escolha_jogador = int(input('Escolha uma carta (digite um número entre 1 e {}): '.format(len(baralho))))
+
+    if verifica == True:
+        movimentos = lista_movimentos_possiveis(baralho, escolha_jogador - 1)
+        if movimentos == []:
+            print('A carta {} não pode ser movida, escolha outra!'.format(baralho[escolha_jogador - 1]))
+            continue
+        elif movimentos == [1, 3]:
+            print('Sobre qual carta você quer empilhar o {} ?'.format(baralho[escolha_jogador - 1]))
+            print('1. {}'.format(baralho[escolha_jogador - 2]))
+            print('2. {}'.format(baralho[escolha_jogador - 4]))
+            decisao = input('Digite o número de sua escolha (1 ou 2): ')
+            if decisao == '1':
+                baralho_atualizado = empilha(baralho, escolha_jogador - 1, escolha_jogador - 2)
+                c = 1
+                for carta in baralho_atualizado:
+                    print('{}. {}'.format(c,carta))
+                    c += 1
+                continue
+            elif decisao == '2':
+                baralho_atualizado = empilha(baralho, escolha_jogador - 1, escolha_jogador - 4)
+                c = 1
+                for carta in baralho_atualizado:
+                    print('{}. {}'.format(c,carta))
+                    c += 1
+                continue
